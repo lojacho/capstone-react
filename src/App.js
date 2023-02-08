7ba64469ec3299166907afe59b04fe7c589b724b
diego <dgcuenca@gmail.com>
2023-02-08 13:37:01 -0500

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/homepage';
 
 function App() {
-  return ('Hello');
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Homepage />}
+      />
+      <Route
+        path="/details/:id/"
+        element={<AnimeCard />}
+      />
+    </Routes>
+  );
 }
 
 export default App;
