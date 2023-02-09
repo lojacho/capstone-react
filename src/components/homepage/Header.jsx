81b84c410ab0cea011391df240d7fb48d6ddac98
diego <dgcuenca@gmail.com>
2023-02-09 12:32:39 -0500

    Display number of animes fetched

--- src/components/homepage/Header.jsx
+++ src/components/homepage/Header.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => (<div>Hello</div>);
+const Header = ({ topAnime }) => {
+  console.log(topAnime);
+  return (
+    <div>
+      <h1>
+        Top Anime
+      </h1>
+      {topAnime.pagination.items.count}
+    </div>
+  );
+};
+
+Header.propTypes = {
+  topAnime: PropTypes.objectOf(PropTypes.object()).isRequired,
+};
 
 export default Header;
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAnime } from '../features/homepage/animes.slice';
 import Header from '../components/homepage/Header';
 
-const homepage = () => (
-  <>
-    <main className="container">
-      <Header />
-    </main>
-  </>
-);
+const Homepage = () => {
+  const dispatch = useDispatch();
+  const animeLoading = useSelector((state) => state.topAnime.loading);
+  const error = useSelector((state) => state.topAnime.error);
+  const topAnime = useSelector((state) => state.topAnime.topAnime);
+  useEffect(() => {
+    if (animeLoading === 'idle') {
+      dispatch(fetchAnime());
+    }
+  }, [dispatch, animeLoading]);
 
-export default homepage;
+  let tobeDisplay = '';
+  if (animeLoading === 'loading') {
+    tobeDisplay = <div>Loading</div>;
+  } else if (animeLoading === 'succeeded') {
+    tobeDisplay = (
+      <main>
+        <Header topAnime={topAnime} />
+      </main>
+    );
+  } else if (animeLoading === 'failed') {
+    tobeDisplay = <em>{error}</em>;
+  }
+  return tobeDisplay;
+};
+
+export default Homepage;
