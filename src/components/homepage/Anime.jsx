0f979af86c1295bf82513fa9f1e8e9275717e5ad
diego <dgcuenca@gmail.com>
2023-02-09 17:48:50 -0500

    insert icon for more information

--- src/components/homepage/Anime.jsx
+++ src/components/homepage/Anime.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { IoArrowForwardCircleOutline } from 'react-icons/io5';
 
 const Anime = ({ topAnimeData }) => (
   topAnimeData.map((anime) => (
-    <li key={anime.mal_id}>
-      <p>{anime.title}</p>
-      <img
-        src={anime.images.jpg.small_image_url}
-        alt={anime.title}
-      />
+    <li
+      key={anime.mal_id}
+      style={{
+        backgroundImage: `url(${anime.images.jpg.large_image_url})`,
+        backgroundSize: '70% 70%',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'top 1.4rem left 0.5rem',
+      }}
+    >
+      <h3>{anime.title}</h3>
+      <div className="forward"><IoArrowForwardCircleOutline /></div>
     </li>
   ))
 );
@@ -20,7 +26,7 @@ Anime.propTypes = {
     rating: PropTypes.string.isRequired,
     images: PropTypes.shape({
       jpg: PropTypes.shape({
-        small_image_url: PropTypes.string.isRequired,
+        large_image_url: PropTypes.string.isRequired,
       }).isRequired,
     }).isRequired,
   })).isRequired,
