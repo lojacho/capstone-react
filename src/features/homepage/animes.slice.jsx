50aed0264534d603aba8fa95075ce3173f7277a0
diego <dgcuenca@gmail.com>
2023-02-09 10:14:52 -0500

    Display header component

--- src/features/homepage/animes.slice.jsx
+++ src/features/homepage/animes.slice.jsx
@@ -1,47 +1,48 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+
 const ANIME_URL = 'https://api.jikan.moe/v4/top/anime';
 // Actions
 const FETCH_ANIME = 'homepage/FETCH_ANIME';
 
 // Action creators
 export const fetchAnime = createAsyncThunk(FETCH_ANIME, async () => {
-    const response = await axios.get(ANIME_URL);
-    const anime = response.data;
-    return anime;
+  const response = await axios.get(ANIME_URL);
+  const anime = response.data;
+  return anime;
 });
 
 // Reducer
 const animeSlice = createSlice({
-    name: 'topAnime',
-    initialState: {
-      topAnime: [],
-      loading: 'idle',
-      error: null,
+  name: 'topAnime',
+  initialState: {
+    topAnime: [],
+    loading: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: {
+    [fetchAnime.pending]: (state) => {
+      const newState = { ...state, loading: 'loading' };
+      return newState;
     },
-    reducers: {},
-    extraReducers: {
-      [fetchAnime.pending]: (state) => {
-        const newState = { ...state, loading: 'loading' };
-        return newState;
-      },
-      [fetchAnime.fulfilled]: (state, action) => {
-        const newState = {
-          ...state,
-          loading: 'succeeded',
-          topAnime: action.payload,
-        };
-        return newState;
-      },
-      [fetchAnime.rejected]: (state, action) => {
-        const newState = {
-          ...state,
-          loading: 'failed',
-          error: action.error.message,
-        };
-        return newState;
-      },
+    [fetchAnime.fulfilled]: (state, action) => {
+      const newState = {
+        ...state,
+        loading: 'succeeded',
+        topAnime: action.payload,
+      };
+      return newState;
     },
-  });
-  
-  export default animeSlice.reducer;
+    [fetchAnime.rejected]: (state, action) => {
+      const newState = {
+        ...state,
+        loading: 'failed',
+        error: action.error.message,
+      };
+      return newState;
+    },
+  },
+});
+
+export default animeSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,9 +12,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
