2ce6d75c734d2a4f137d315c568149e4a196b37b
diego <dgcuenca@gmail.com>
2023-02-10 12:02:56 -0500

    Import state from store

--- src/pages/animeDetail.jsx
+++ src/pages/animeDetail.jsx
@@ -1,13 +1,21 @@
 import { useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const AnimeDetail = () => {
+const mapStateToProps = (state) => ({
+  topAnime: state.topAnime,
+});
+
+const AnimeDetailNotConectect = ({ topAnime }) => {
   const { index } = useParams();
   console.log(index, 'Index coming from URL');
+  console.log(topAnime, 'anime detail');
   return (
     <div>
       Perfil del Usuario
     </div>
   );
 };
+
+const AnimeDetail = connect(mapStateToProps)(AnimeDetailNotConectect);
 
 export default AnimeDetail;
