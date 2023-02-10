7943b0f698ae3804916689b53cfd55c1e02a5e20
diego <dgcuenca@gmail.com>
2023-02-10 17:45:12 -0500

    Add test for render title from fetch

---
+++ src/components/homepage/Header.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import Header from './Header';
+
+test('renders qty to be render', () => {
+  const topAnime = {
+    pagination: {
+      items: {
+        count: 25,
+      }
+    },
+  }
+
+  const mockStore = configureStore();
+  const store = mockStore(topAnime);
+  const component = render(<Provider store={store}><Header topAnime={topAnime} /></Provider>);
+  // since reserved is passing as true
+  expect(component.container).toHaveTextContent(topAnime.pagination.items.count);
+});
